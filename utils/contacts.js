7f62edf56d2be5240fde58651dd2544e88024b85
lizaiueo <elizabethfrwj@gmail.com>
2024-09-10 17:18:35 +0700

    contactapp

---
+++ utils/contacts.js
@@ -0,0 +1,72 @@
+
+const fs = require('fs');
+
+
+//Membuat folder data
+const dirPath = './data';
+if(!fs.existsSync(dirPath)){
+fs.mkdirSync(dirPath);
+}
+
+//Membuat file contact.json jika belum ada
+const dataPath = './data/contacts.json';
+if(!fs.existsSync(dataPath)){
+    fs.writeFileSync(dataPath, '[]', 'utf-8');
+}
+
+
+//ambil semua data contact.json
+const loadContact = () => {
+    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
+    const contacts = JSON.parse(fileBuffer);
+    return contacts;
+};
+
+//cari kontak berdasarkan nama
+const findContact = (nama ) => {
+    const contacts = loadContact();
+    const contact = contacts.find(
+        (contact) => contact.nama.toLowerCase() === nama.toLowerCase());
+        return contact;
+    };
+
+
+    //menuliskan /menimpa file contacts.json dengan data yang baru
+    const saveContacts = (contacts) => {
+        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+    }
+
+    //menambahkan data contact baru
+    const addContact = (contact) => {
+        const contacts = loadContact();
+        contacts.push(contact);
+        saveContacts(contacts);
+    }
+
+    //cek nama yang duplikat
+
+    const cekDuplikat = (nama) => {
+        const contacts = loadContact();
+        return contacts.find((contact) => contact.nama === nama);
+    };
+
+    //hapus contact
+    const deleteContact= (nama) => {
+        const contacts = loadContact();
+        const filteredContacts = contacts.filter((contact) => contact.nama !== nama);
+        saveContacts(filteredContacts);
+    };
+
+    //mengubah contacts
+    const updateContacts = (contactBaru) => {
+        const contacts = loadContact();
+
+        //hilangkan kontak lama yang namanya sama dengan oldnama
+        const filteredContacts = contacts.filter((contact) => contact.nama !== contactBaru.oldNama);
+        // console.log(filteredContacts, contactBaru);
+        delete contactBaru.oldNama;
+        filteredContacts.push(contactBaru);
+        saveContacts(filteredContacts);
+    };
+
+module.exports = {loadContact, findContact, addContact, cekDuplikat, deleteContact, updateContacts};
